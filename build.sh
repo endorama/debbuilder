5ce039a9c410309d7875a14b3f1d5ed716508fdf
Edoardo Tenani <edoardo.tenani@gmail.com>
2018-03-03 02:47:00 +0100

    fix build script to properly handle package folder

--- build.sh
+++ build.sh
@@ -6,23 +6,23 @@ package=$1
 platform=$2
 version=${3:-"v1.2.2"}
 
-echo "Building lastpass-cli version $version for $platform"
+echo "Building lastpass-cli version ${version} for ${platform}"
 
-pushd src
+pushd "${package}/src"
 git checkout $version
 popd
 
-release=$(cat ./$platform/release)
+release=$(cat ./${package}/${platform}/release)
 release=$(($release + 1))
 
 docker run \
 	-v $(pwd):/buildroot\
-	-e LASTPASS_VERSION="${version:1}" \
+	-e LASTPASS_VERSION="${version}" \
 	-e PACKAGE_RELEASE_NUMBER="${release}" \
 	--rm \
 	-i \
-	-t debbuilder-lastpass-cli:$platform \
-	/bin/bash -c /buildroot/$platform/build.sh
+	-t "debbuilder-lastpass-cli:${platform}" \
+	/bin/bash -c "/buildroot/${platform}/build.sh"
 
 echo $release > "./${platform}/release"
 
