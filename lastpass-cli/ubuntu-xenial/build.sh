55e64726224b6d5f2d8719d7803c7ea9afcf95ce
Edoardo Tenani <edoardo.tenani@gmail.com>
2018-03-26 23:26:39 +0200

    refactor lastpass-cli

--- lastpass-cli/ubuntu-xenial/build.sh
+++ lastpass-cli/ubuntu-xenial/build.sh
@@ -4,24 +4,44 @@ set -euo pipefail
 
 package_name="lastpass-cli"
 package_base_path="/buildroot/${package_name}"
+deb_version=${PACKAGE_VERSION}-0ubuntu${PACKAGE_RELEASE_NUMBER}
+
+checkout_git_version() {
+	git checkout "v${PACKAGE_VERSION}"
+}
+
+update_changelog() {
+	cat <<EOS > debian/changelog.new
+lastpass-cli (${deb_version}) $(lsb_release -cs); urgency=medium
+
+  * Manual package build for PPA, git tag v${PACKAGE_VERSION}
+
+ -- Edoardo Tenani <edoardo.tenani@protonmail.com>  $(date -R)
+
+EOS
+	cat debian/changelog >> debian/changelog.new
+	mv debian/changelog.new debian/changelog
+}
+
+git_tag_relase() {
+	local version
+	version=$1
+	git config user.email "edoardo.tenani@protonmail.com"
+	git config user.name "Edoardo Tenani"
+	git add debian/changelog
+	git commit -m "update changelog"
+	git tag | grep ${version} && git tag -d ${version}
+	git tag ${version}
+}
+
+build_deb() {
+	debuild --no-tgz-check -S -sa
+}
+
 
 cd  "${package_base_path}/src"
 
-cp "${package_base_path}/description-pak" description-pak
-
-checkinstall -y \
-	--install=no \
-	--type debian \
-	--pkgname "${package_name}" \
-	--pkgversion "${LASTPASS_VERSION}" \
-	--pkgrelease "0ubuntu${PACKAGE_RELEASE_NUMBER}" \
-	--pkglicense GPLv2 \
-	--provides lpass \
-	--maintainer "edoardo.tenani@protonmail.com" \
-	--pakdir "/buildroot/dist" \
-	--backup=no \
-	make install-doc install
-
-chown 1000:1000 /buildroot/dist/*
-
-rm -fr *-pak
+checkout_git_version
+update_changelog
+git_tag_relase ${deb_version}
+build_deb
