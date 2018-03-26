532a55aee71e1a429f9147c8799a758b118b7990
Edoardo Tenani <edoardo.tenani@gmail.com>
2018-03-26 23:24:33 +0200

    refactor docker images builder

--- build-environment.sh
+++ build-environment.sh
@@ -5,8 +5,13 @@ set -euo pipefail
 package=$1
 platform=$2
 
+tag=$(date +%Y%m%d%H%M%S)
+
+docker rmi "debbuilder-${package}-${platform}:latest"
+
 docker build \
-	--label "debbuilder-${package}" \
-	--tag "${platform}" \
+	--tag "debbuilder-${package}-${platform}:${tag}" \
+	--tag "debbuilder-${package}-${platform}:latest" \
 	--file "${package}/${platform}/Dockerfile" \
-	--rm
+	--rm --compress \
+	${package}
